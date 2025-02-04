a7dae81e78d7a78397815217aa9bfbf26733169f
Gopal <139344893+gopalGithubsample@users.noreply.github.com>
2025-02-04 16:00:29 +0530

    commit
    Day 4

--- wipro_java_project/src/com/wipro/java/exception/ExceptionHandling.java
+++ wipro_java_project/src/com/wipro/java/exception/ExceptionHandling.java
@@ -5,7 +5,8 @@ public class ExceptionHandling {
 
     static int l = 2; // It has class-level declaration.
 
-    public static void main(String args[]) {
+    @SuppressWarnings("unused")
+	public static void main(String args[]) {
         int i = 50, j = 0;
 
         try {
@@ -13,13 +14,8 @@ public class ExceptionHandling {
             int a[] = new int[5]; // Array is a collection of the same type of object.
             a[5] = 30 / j; // This will cause both an ArrayIndexOutOfBoundsException AND ArithmeticException.  The ArrayIndexOutOfBoundsException will be thrown first.
            
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array Index Out of Bounds Exception occurred: " + e.toString());
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception (Division by zero) occurred: " + e.toString());
-        } catch (Exception e) { // Generic exception handler (good practice to include).
-            System.out.println("Some other exception occurred: " + e.toString());
-           
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.toString());
         } finally {
             System.out.println("Finally block: I am always executed.");
         }
