697ce5e8c6002882ca43723b458d7a6b7a17f42c
Gopal <139344893+gopalGithubsample@users.noreply.github.com>
2025-02-05 16:43:39 +0530

    commit
    day 5

--- wipro_java_project/src/com/wipro/java/collection/ListOperation.java
+++ wipro_java_project/src/com/wipro/java/collection/ListOperation.java
@@ -7,16 +7,28 @@ public class ListOperation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Creating an Object of List interface
+		//implemented by the ArrayList class 
+		
 		List<Integer> l1 = new ArrayList<Integer>();
 		
-		l1.add(0,1);
+		//Adding elements to object of List interface
+		//Custom inputs
+		//Addition is different from replacement
+		
+		l1.add(0,1); //Position , Value
 		l1.add(1,2);
 		l1.add(2,3);
+		
+		//Print the elements inside object
 		System.out.println(l1);
 		
-List<Integer> l2 = new ArrayList<Integer>();
 		
-		l2.add(1);
+		List<Integer> l2 = new ArrayList<Integer>();
+		
+		
+		
+		l2.add(1); 
 		l2.add(2);
 		l2.add(3);
 		
